a7d2528c294c2957eee8610a37790a3329d90659
Fathimath Nafiya <fathimathnafiya500@gmail.com>
2026-06-12 16:05:10 +0530

    Fix app routes after merge

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ console.log("DB URL:", process.env.DATABASE_URL);
 console.log("JWT:", process.env.JWT_SECRET);
 
 const express = require("express");
-
 const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
@@ -14,25 +13,22 @@ const courseRoutes = require("./routes/courseRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const tutorProfileRoutes = require("./routes/tutorProfileRoutes");
 const uploadRoutes = require("./routes/uploadRoutes");
-const authMiddleware = require("./middlewares/authMiddleware");
-<<<<<<< HEAD
 const certificateRoutes = require("./routes/certificateRoutes");
-=======
 const progressRoutes = require("./routes/progressRoutes");
->>>>>>> f1cce55a8006df1adc69fd06fe749dda2b7273d5
 
+const authMiddleware = require("./middlewares/authMiddleware");
 
 const app = express();
+
 app.use(cors({
   origin: [
+    "http://localhost:3000",
     "https://refactored-space-telegram-g4964j7p9prghp747-3000.app.github.dev"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-
-
 
 app.use(express.json());
 
@@ -47,7 +43,6 @@ app.get("/api/protected", authMiddleware, (req, res) => {
   });
 });
 
-
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/courses", courseRoutes);
@@ -56,9 +51,11 @@ app.use("/api/tutor-profile", tutorProfileRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/certificates", certificateRoutes);
 app.use("/api/progress", progressRoutes);
+
 app.get("/test", (req, res) => {
   res.send("Test route works");
 });
+
 app.listen(5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
